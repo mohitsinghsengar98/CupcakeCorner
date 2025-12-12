03383908c8e76ec567d0629136a4a52fb631df10
Mohit Singh Sengar <mssengar.988@gmail.com>
2025-12-12 22:02:11 +0530

    having loaded, error, loading phases while working with remote image load.

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -10,12 +10,16 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        AsyncImage(url: URL(string:"https://hws.dev/img/logo.png")){ image in
-            image
-                .resizable()
-                .scaledToFit()
-        }placeholder:{
-            ProgressView()
+        AsyncImage(url: URL(string:"https://hws.dev/img/logo.png")){phase in
+            if let image = phase.image{
+                image
+                    .resizable()
+                    .scaledToFit()
+            }else if phase.error != nil{
+                Text("There was an error loading image from the server.")
+            }else{
+                ProgressView()
+            }
         }
         .frame(width:200,height:200)
     }
